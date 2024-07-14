6c2ae9ba6b0ad02a60215e94db5f8f1dd066c034
MJ Uyheng <mj@symph.co>
2024-07-15 01:05:49 +0800

    [chore] Disable reactStrictMode
    Disable reactStrictMode due to a rerender issue
    causing components like the Navbar to display multiple
    times on rerender.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	output: "export",
+	reactStrictMode: false, // Disabling for now because of rerender issue. See: https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
 };
 
 export default nextConfig;
