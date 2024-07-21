10dccbf00ecaf2df272f4431d3b32ed9546caf61
TinoDev <tinomandreza.dev@gmail.com>
2024-07-21 15:16:35 +0800

    [feature] SSI-11 Add .yml files to allow GitHub Actions deployment
    - Files should allow us to auto-deploy changes to tilmodev.github.io
    
    Guide used:
    https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	basePath: "/sellswords",	// Changes the app to be seen in tilmodev.githhub.io/sellswords
 	output: "export",
-	reactStrictMode: false, // Disabling for now because of rerender issue. See: https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
+	reactStrictMode: false,		// Disabling for now because of rerender issue. See: https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
 };
 
 export default nextConfig;
