8888899c90def4cdd31d7cd9520dcddd1893c55e
Justin Uyheng <justinuyheng23@gmail.com>
2025-09-09 01:00:50 +0800

    Reintroduce /sellswords as base path to resolve build warning about generating relative links.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: "",
+	basePath: "/sellswords", // Changes the app to be seen in tilmodev.githhub.io/sellswords
 	output: "export",
 	reactStrictMode: false, // Disabling for now because of rerender issue. See: https://stackoverflow.com/questions/71847778/why-my-nextjs-component-is-rendering-twice
 	images: {
